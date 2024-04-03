536daa87345bfe50708d928cfdb139674b35780a
Mayank Patel <91795753+MayankPatel7@users.noreply.github.com>
2024-04-03 22:28:22 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,24 @@
+import requests
+
+print("Welcome to weather app")
+city = input("Enter the city name: ")
+
+def get_request(city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    api_key = YOUR API KEY in string
+    url = base_url+"appid="+api_key+"&q="+city+"&units=metric"
+    data = requests.get(url).json()
+
+    name = data['name']
+    coord = {"Longitudes" : data['coord']['lon'], "Latitudes" : data['coord']['lat']}
+    temp = str(data['main']['temp'])+"° C"
+    desc = data['weather'][0]['description']
+    wind = str(data['wind']['speed'])+"kmph"
+
+    print("City name:", name)
+    print("Coordiates:", coord)
+    print("Temperature:", temp)
+    print("Description:", desc)
+    print("Wind Speed:", wind)
+
+get_request(city)
